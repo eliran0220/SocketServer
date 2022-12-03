0a9bc7970f8caa83fe0a0d85574d3250dc4a9b3d
ELIRAN\Eliran <eliran0220@gmail.com>
2022-12-03 02:27:43 +0200

    changed build folder

--- build/server.js
+++ build/server.js
@@ -38,82 +38,112 @@ class RedisServer {
         }
     };
     initIncomingConnection = async () => {
-        this.io.on("connection", async (socket) => {
-            console.log(`${socket.id} has been connected!`);
-            const emitter = this.emitter.in(socket.id);
-            const response = { message: "connected" };
-            emitter.emit("data", response);
-            await this.handleRequests(socket);
-        });
+        try {
+            this.io.on("connection", async (socket) => {
+                console.log(`${socket.id} has been connected!`);
+                const emitter = this.emitter.in(socket.id);
+                const response = { message: "connected" };
+                emitter.emit("data", response);
+                await this.handleRequests(socket);
+            });
+        }
+        catch (err) {
+            console.log("err at connecting socket");
+        }
     };
     handleRequests = async (socket) => {
-        socket.on("spin", async (data) => {
-            console.log("onSpin");
-            utilities_1.default.validateRequest(data);
-            await this.handleSpin(data);
-        });
-        socket.on("wild", async (data) => {
-            console.log("onWild");
-            utilities_1.default.validateRequest(data);
-            await this.handleWild(data);
-        });
-        socket.on("blast", async (data) => {
-            console.log("onBlast");
-            utilities_1.default.validateRequest(data);
-            await this.handleBlast(data);
-        });
-        socket.on("disconnect", async (data) => {
-            console.log("onDisconnect");
-            await this.handleDisconnect(data, socket);
-        });
+        try {
+            socket.on("spin", async (data) => {
+                console.log("onSpin");
+                utilities_1.default.validateRequest(data);
+                await this.handleSpin(data);
+            });
+            socket.on("wild", async (data) => {
+                console.log("onWild");
+                utilities_1.default.validateRequest(data);
+                await this.handleWild(data);
+            });
+            socket.on("blast", async (data) => {
+                console.log("onBlast");
+                utilities_1.default.validateRequest(data);
+                await this.handleBlast(data);
+            });
+            socket.on("disconnect", async (data) => {
+                console.log("onDisconnect");
+                await this.handleDisconnect(data, socket);
+            });
+        }
+        catch (err) {
+            console.log(err);
+        }
     };
     handleSpin = async (data) => {
-        console.log("rooms");
-        const rooms = [...await this.io.of('/').adapter.allRooms()];
-        const random_connection = utilities_1.default.getRandomConnection(rooms);
-        const response = { message: data.message };
-        const emitter = this.emitter.in(random_connection);
-        emitter.emit("data", response);
-    };
-    handleWild = async (data) => {
-        const response = { message: data.message };
-        if (!data.random_number || data.random_number === 0) {
-            return;
-        }
-        let random = 0;
-        if (!Number.isFinite(data.random_number)) {
-            // put default value 1 in case the random_number is not in number value, simple check
-            random = 1;
-        }
-        const rooms = [...await this.io.of('/').adapter.allRooms()];
-        if (random === 1) {
+        try {
+            console.log("rooms");
+            const rooms = [...await this.io.of('/').adapter.allRooms()];
             const random_connection = utilities_1.default.getRandomConnection(rooms);
+            const response = { message: data.message };
             const emitter = this.emitter.in(random_connection);
             emitter.emit("data", response);
-            return;
         }
-        else if (random >= rooms.length) {
-            // in case number bigger than connections size was provided, emit to all
-            this.emitter.emit("data", response);
-            return;
+        catch (err) {
+            console.log("err att spin");
         }
-        else {
+    };
+    handleWild = async (data) => {
+        try {
+            const response = { message: data.message };
+            if (!data.random_number || data.random_number === 0) {
+                return;
+            }
+            let random = 0;
+            if (!Number.isFinite(data.random_number)) {
+                // put default value 1 in case the random_number is not in number value, simple check
+                random = 1;
+            }
             const rooms = [...await this.io.of('/').adapter.allRooms()];
-            const cloned_connectios = [...rooms];
-            let left = Number(data.random_number);
-            const chosen_connections = utilities_1.default.getRandomConnections(left, cloned_connectios);
-            this.emitChosenConnections(chosen_connections, response);
+            if (random === 1) {
+                const random_connection = utilities_1.default.getRandomConnection(rooms);
+                const emitter = this.emitter.in(random_connection);
+                emitter.emit("data", response);
+                return;
+            }
+            else if (random >= rooms.length) {
+                // in case number bigger than connections size was provided, emit to all
+                this.emitter.emit("data", response);
+                return;
+            }
+            else {
+                const rooms = [...await this.io.of('/').adapter.allRooms()];
+                const cloned_connectios = [...rooms];
+                let left = Number(data.random_number);
+                const chosen_connections = utilities_1.default.getRandomConnections(left, cloned_connectios);
+                this.emitChosenConnections(chosen_connections, response);
+            }
+        }
+        catch (err) {
+            console.log("err at wild");
         }
     };
     emitChosenConnections = async (chosen_connections, response) => {
-        let wildEmitter = null;
-        for (const connection of chosen_connections) {
-            wildEmitter = this.emitter.in(connection);
-            wildEmitter.emit("data", response);
+        try {
+            let wildEmitter = null;
+            for (const connection of chosen_connections) {
+                wildEmitter = this.emitter.in(connection);
+                wildEmitter.emit("data", response);
+            }
+        }
+        catch (err) {
+            console.log("err at emitChosenConnections");
         }
     };
     handleBlast = async (data) => {
-        this.emitter.emit("data", data);
+        try {
+            this.emitter.emit("data", data);
+        }
+        catch (err) {
+            console.log("err at blast");
+        }
     };
     handleDisconnect = async (data, socket) => {
         console.log(`${socket.id} has been disconnected.`);
